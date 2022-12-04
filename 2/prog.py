b65431fca6e16a58f21a28b53895fd109ede24f1
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2022-12-04 01:06:51 +0100

    Day 2 - Part 2

--- 2/prog.py
+++ 2/prog.py
@@ -14,6 +14,15 @@ MOVE_TABLE = {
 	"Z": SCISSORS
 }
 
+OUTCOME_LOSE = "LOSE"
+OUTCOME_DRAW = "DRAW"
+OUTCOME_WIN = "WIN"
+OUTCOME_TABLE = {
+	"X": OUTCOME_LOSE,
+	"Y": OUTCOME_DRAW,
+	"Z": OUTCOME_WIN
+}
+
 MOVE_SCORE = {
 	ROCK: 1,
 	PAPER: 2,
@@ -39,19 +48,60 @@ def execute_round(opponent_move:str, my_move:str):
 
 	return ROUND_WON_SCORE
 
-def main():
-	f = open("input.txt")
-	if not f:
-		return 1
+def determine_move(opponent_move: str, desired_outcome: str):
+	if desired_outcome == OUTCOME_DRAW:
+		return opponent_move
 
+	if desired_outcome == OUTCOME_WIN:
+		if opponent_move == ROCK:
+			return PAPER
+		if opponent_move == PAPER:
+			return SCISSORS
+		if opponent_move == SCISSORS:
+			return ROCK
+	if desired_outcome == OUTCOME_LOSE:
+		if opponent_move == ROCK:
+			return SCISSORS
+		if opponent_move == PAPER:
+			return ROCK
+		if opponent_move == SCISSORS:
+			return PAPER
+
+	return SCISSORS
+
+
+def part_one(lines):
 	score = 0
-	for line in f.readlines():
+	for line in lines:
 		moves = line.strip().split(" ")
 		opponent_move = MOVE_TABLE[moves[0]]
 		my_move = MOVE_TABLE[moves[1]]
 		score += execute_round(opponent_move, my_move) + MOVE_SCORE[my_move]
 
-	print("Final Score: "+str(score))
+	print("Part 1 - Final Score: "+str(score))
+
+def part_two(lines):
+	score = 0
+	for line in lines:
+		data = line.strip().split(" ")
+		opponent_move = MOVE_TABLE[data[0]]
+		outcome = OUTCOME_TABLE[data[1]]
+
+		my_move = determine_move(opponent_move, outcome)
+
+		score += execute_round(opponent_move, my_move) + MOVE_SCORE[my_move]
+
+	print("Part 2 - Final Score: "+str(score))
+
+def main():
+	f = open("input.txt")
+	if not f:
+		return 1
+
+	lines = f.readlines()
+	part_one(lines)
+	part_two(lines)
+
 	f.close()
 	return 0
 
